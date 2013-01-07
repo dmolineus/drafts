da07291642e225c00ec02b059bf070e355205c79
David Molineus <support@marmalade.de>
2013-01-07 11:19:08 +0100

    minor changes

--- datacontainers/Content.php
+++ datacontainers/Content.php
@@ -113,8 +113,8 @@ class Content extends DraftableDataContainer
 			$GLOBALS['TL_DCA'][$this->strTable]['list']['sorting']['child_record_callback'] = array($strClass, 'generateChildRecord');
 			
 			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['toggle']['attributes']		= 'onclick="Backend.getScrollOffset();AjaxRequest.toggleVisibility(this,%s);return toggleDraftLabel(this, \'visibility\', \'' . urlencode($GLOBALS['TL_LANG'][$this->strTable]['draftState_visibility']) . '\')"';
-			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['toggle']['button_callback'] = array($strClass, 'generateButtonToggle');
-			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['toggle']['button_rules']	= array('toggleIcon:field=invisible:inverted', 'generate');
+			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['toggle']['button_callback'] 	= array($strClass, 'generateButtonToggle');
+			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['toggle']['button_rules']		= array('toggleIcon:field=invisible:inverted', 'generate');
 		}
 	
 		// check permission for operations in live mode
@@ -132,10 +132,10 @@ class Content extends DraftableDataContainer
 			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['edit']['button_rules']		= array('hasAccessOnPublished', 'generate');
 			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['copy']['button_callback'] 	= array($strClass, 'generateButtonCopy');
 			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['copy']['button_rules']		= array('hasAccessOnPublished', 'generate');
-			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['delete']['button_callback'] = array($strClass, 'generateButtonDelete');
-			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['delete']['button_rules']	= array('hasAccessOnPublished', 'generate');
-			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['toggle']['button_callback'] = array($strClass, 'generateButtonToggle');
-			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['toggle']['button_rules']	= array('hasAccessOnPublished:icon=invisible.gif', 'toggleIcon:field=invisible:inverted', 'generate');
+			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['delete']['button_callback'] 	= array($strClass, 'generateButtonDelete');
+			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['delete']['button_rules']		= array('hasAccessOnPublished', 'generate');
+			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['toggle']['button_callback'] 	= array($strClass, 'generateButtonToggle');
+			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['toggle']['button_rules']		= array('hasAccessOnPublished:icon=invisible.gif', 'toggleIcon:field=invisible:inverted', 'generate');
 			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['cut']['button_callback'] 	= array($strClass, 'generateButtonCut');
 			$GLOBALS['TL_DCA'][$this->strTable]['list']['operations']['cut']['button_rules']		= array('hasAccessOnPublished', 'generate');
 		}
