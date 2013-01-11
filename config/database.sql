6b509c7dd06be3c28ee89ffc8e24312bd2d86166
David Molineus <support@marmalade.de>
2013-01-11 09:17:36 +0100

    make sure that draftid can be added to tl_task

--- config/database.sql
+++ config/database.sql
@@ -1,3 +1,5 @@
 CREATE TABLE `tl_task` (
-      `draftsid` int(10) unsigned NOT NULL default '0'
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `draftsid` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
