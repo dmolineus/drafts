6d2a5258d0c69cee6ead73c4bd70b016d3de7867
David Molineus <support@marmalade.de>
2013-01-15 22:44:08 +0100

    make sure that options preparation also work on live mode

--- models/ContentModel.php
+++ models/ContentModel.php
@@ -21,54 +21,59 @@ class ContentModel extends Contao\ContentModel
 	 * @param array
 	 */
 	protected static function find(array $arrOptions)
-	{		
+	{
+		$blnPreview = TL_MODE == 'FE' && \Input::cookie('DRAFT_MODE') == '1';
 		$t = static::$strTable;
 		
-		// get all draft elements
-		if(TL_MODE == 'FE' && \Input::cookie('DRAFT_MODE') == '1')
+		if(!isset($arrOptions['column']))
 		{
-			if(!isset($arrOptions['column']))
+			$arrOptions['column'] = array();
+		}
+		elseif(!is_array($arrOptions['column']))
+		{
+			// try to find related one
+			// useful for ContentAlias for example
+			if($blnPreview && $arrOptions['column'] == 'id')
 			{
-				$arrOptions['column'] = array();
-			}
-	 
-			elseif(!is_array($arrOptions['column']))
-			{
-				// try to find related one
-				// useful for ContentAlias for example
-				if($arrOptions['column'] == 'id')
-				{
-					$arrNew = $arrOptions;
-					$arrNew['column'] = 'draftRelated';
-					
-					$objReturn = parent::find($arrNew);
-					
-					if($objReturn !== null)
-					{
-						return $objReturn;
-					}
-				}
+				$arrNew = $arrOptions;
+				$arrNew['column'] = 'draftRelated';
 				
-				// load dca extrator so data container is loaded, can not use loadDataContainer is static context
-				$objDca = new \DcaExtractor($t);
-				$strKey = $arrOptions['column'];
-				$arrKeys = $GLOBALS['TL_DCA'][$t]['config']['sql']['keys'];
-	
-				// do not add draftState if column is unique			
-				if((isset($arrKeys[$strKey]) && ($arrKeys[$strKey] == 'unique' || $arrKeys[$strKey] == 'primary')) || $GLOBALS['TL_DCA'][$t]['fields'][$strKey]['eval']['unique'])
-				{
-					return parent::find($arrOptions);				
-				}
+				$objReturn = parent::find($arrNew);
 				
-				$arrOptions['column'] = array($arrOptions['column']);
+				if($objReturn !== null)
+				{
+					return $objReturn;
+				}
 			}
 			
+			// load dca extrator so data container is loaded, can not use loadDataContainer is static context
+			$objDca = new \DcaExtractor($t);
+			$strKey = $arrOptions['column'];
+			$arrKeys = $GLOBALS['TL_DCA'][$t]['config']['sql']['keys'];
+
+			// do not add draftState if column is unique			
+			if((isset($arrKeys[$strKey]) && ($arrKeys[$strKey] == 'unique' || $arrKeys[$strKey] == 'primary')) || $GLOBALS['TL_DCA'][$t]['fields'][$strKey]['eval']['unique'])
+			{
+				return parent::find($arrOptions);				
+			}
+			
+			$arrOptions['column'] = array($arrOptions['column']);
+		}
+			
+			
+		// get all draft elements
+		if($blnPreview)
+		{
 			$arrOptions['column'][] = "(($t.draftState = 0 AND $t.draftRelated IS NULL) OR $t.draftState > 0)";			
 		}
 		
 		// limit to live elements by default
 		else 
 		{
+			if(!is_array($arrOptions['column']))
+			{
+				$arrOptions['column'] = array($arrOptions['column']);
+			}
 			$arrOptions['column'][] = "$t.draftState = 0";
 		}
 		
