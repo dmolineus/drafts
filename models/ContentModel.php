8d22b4866eda5130f133a8db36709dd7f911cbbf
David Molineus <support@marmalade.de>
2013-01-15 22:36:47 +0100

    support pk switching to draft mode

--- models/ContentModel.php
+++ models/ContentModel.php
@@ -24,34 +24,48 @@ class ContentModel extends Contao\ContentModel
 	{		
 		$t = static::$strTable;
 		
-		if(!isset($arrOptions['column']))
-		{
-			$arrOptions['column'] = array();
-		}
- 
-		elseif(!is_array($arrOptions['column']))
-		{
-			// load dca extrator so data container is loaded, can not use loadDataContainer is static context
-			$objDca = new \DcaExtractor($t);
-			$strKey = $arrOptions['column'];
-			$arrKeys = $GLOBALS['TL_DCA'][$t]['config']['sql']['keys'];
-
-			// do not add draftState if column is unique			
-			if((isset($arrKeys[$strKey]) && ($arrKeys[$strKey] == 'unique' || $arrKeys[$strKey] == 'primary')) || $GLOBALS['TL_DCA'][$t]['fields'][$strKey]['eval']['unique'])
-			{
-				return parent::find($arrOptions);				
-			}
-			
-			$arrOptions['column'] = array($arrOptions['column']);
-		}
-		
 		// get all draft elements
 		if(TL_MODE == 'FE' && \Input::cookie('DRAFT_MODE') == '1')
 		{
+			if(!isset($arrOptions['column']))
+			{
+				$arrOptions['column'] = array();
+			}
+	 
+			elseif(!is_array($arrOptions['column']))
+			{
+				// try to find related one
+				if($arrOptions['column'] == 'id')
+				{
+					$arrNew = $arrOptions;
+					$arrNew['column'] = 'draftRelated';
+					
+					$objReturn = parent::find($arrNew);
+					
+					if($objReturn !== null)
+					{
+						return $objReturn;
+					}
+				}
+				
+				// load dca extrator so data container is loaded, can not use loadDataContainer is static context
+				$objDca = new \DcaExtractor($t);
+				$strKey = $arrOptions['column'];
+				$arrKeys = $GLOBALS['TL_DCA'][$t]['config']['sql']['keys'];
+	
+				// do not add draftState if column is unique			
+				if((isset($arrKeys[$strKey]) && ($arrKeys[$strKey] == 'unique' || $arrKeys[$strKey] == 'primary')) || $GLOBALS['TL_DCA'][$t]['fields'][$strKey]['eval']['unique'])
+				{
+					return parent::find($arrOptions);				
+				}
+				
+				$arrOptions['column'] = array($arrOptions['column']);
+			}
+			
 			$arrOptions['column'][] = "(($t.draftState = 0 AND $t.draftRelated IS NULL) OR $t.draftState > 0)";			
 		}
 		
-		// filter all draft elements by default
+		// limit to live elements by default
 		else 
 		{
 			$arrOptions['column'][] = "$t.draftState = 0";
