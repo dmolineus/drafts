126ca670796783e9964ab8252f8601d68a4c20b8
David Molineus <support@marmalade.de>
2013-01-17 13:05:58 +0100

    make deadline field only visible if task module is used

--- dca/tl_settings.php
+++ dca/tl_settings.php
@@ -12,7 +12,9 @@
  *  
  **/
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{drafts_legend},draftModules,draftUseTaskModule,draftTaskDefaultDeadline';
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{drafts_legend},draftModules,draftUseTaskModule';
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['__selector__'][] = 'draftUseTaskModule';
+$GLOBALS['TL_DCA']['tl_settings']['subpalettes']['draftUseTaskModule'] = 'draftTaskDefaultDeadline';
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['draftModules'] = array
 (
@@ -27,7 +29,7 @@ $GLOBALS['TL_DCA']['tl_settings']['fields']['draftUseTaskModule'] = array
 (
 	'label'			=> &$GLOBALS['TL_LANG']['tl_settings']['draftUseTaskModule'],
 	'inputType'		=> 'checkbox',
-	'eval'			=> array('tl_class' => 'w50 m12'),
+	'eval'			=> array('tl_class' => 'w50 m12', 'submitOnChange' => true),
 );
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['draftTaskDefaultDeadline'] = array
@@ -35,5 +37,5 @@ $GLOBALS['TL_DCA']['tl_settings']['fields']['draftTaskDefaultDeadline'] = array
 	'label'			=> &$GLOBALS['TL_LANG']['tl_settings']['draftsTaskDefaultDeadline'],
 	'inputType'		=> 'text',
 	'default' 		=> '1',
-	'eval'			=> array('tl_class' => 'w50 clr', 'rgxp' => 'digit', 'maxlength' => 3),
+	'eval'			=> array('tl_class' => 'w50', 'rgxp' => 'digit', 'maxlength' => 3),
 );
