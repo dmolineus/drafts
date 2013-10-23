3ab9f730a0ab028fd053ec85547430a6a67541e7
David Molineus <mail@netzmacht.de>
2013-10-23 11:32:22 +0200

    add model files

---
+++ src/modules/drafts/classes/Drafts/Model/DraftableModel.php
@@ -0,0 +1,286 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package   drafts
+ * @author    David Molineus <http://www.netzmacht.de>
+ * @license   GNU/LGPL 
+ * @copyright Copyright 2012 David Molineus netzmacht creative 
+ *  
+ **/
+
+namespace Drafts\Model;
+
+/**
+ * DraftableModel provides a common interface which can handle draft features
+ * for every model
+ * 
+ */
+class DraftableModel extends VersioningModel
+{
+	
+	/**
+	 * collection class
+	 * 
+	 * @var string
+	 */
+	protected static $strCollectionClass = 'DraftableCollection';
+	
+	
+	/**
+	 * check if table is draftable
+	 * 
+	 * @param string|\Model $objModel
+	 * @param \Database\Result|Model|null
+	 * @param bool $blnVersioning enable versioning
+	 * @throws \RuntimeException if table is not draftable
+	 */
+	public function __construct($objModel, $blnVersioning=false, $strTable=null)
+	{
+		if($strTable === null)
+		{
+			if($objModel instanceof \Model)
+			{
+				$strTable = $objModel->getTable();
+			}
+			else
+			{
+				$strTable = $objModel;
+			}
+		}
+		
+		parent::__construct($objModel, $blnVersioning, $strTable);
+
+		// call it after
+		if(!isset($GLOBALS['TL_DCA'][$strTable]['fields']['draftRelated']))
+		{
+			throw new \RuntimeException('Table "' . $strTable . '" is not draftable.');
+		}
+	}
+	
+	
+	/**
+	 * make sure that ptable cannot be empty for contaos behavior handling
+	 * empty ptables
+	 * 
+	 * @param string key
+	 * @param mixed
+	 */
+	public function __get($strKey)
+	{
+		$strValue = parent::__get($strKey);
+		
+		// and again dynamic ptable backwards compatibility
+		if($strKey == 'ptable' && $strValue == '' && $this->objModel->getTable() == 'tl_content')
+		{
+			return 'tl_article';
+		}
+		
+		return $strValue;
+	}
+	
+	
+	/**
+	 * delete and unset relation
+	 * 
+	 * @return int
+	 */
+	public function delete()
+	{
+		$intAffectedRows = parent::delete();
+		
+		if($intAffectedRows > 0 && $this->hasRelated())
+		{
+			// do not use getRelated because it could be cached, @see #5248
+			$objRelated = new static($this->objModel->getTable());
+			$objRelated->id = $this->draftRelated;
+			$objRelated->draftRelated = 0;			
+			$objRelated->save();
+		}
+		
+		return $intAffectedRows;
+	}
+	
+	
+	/**
+	 * get draftRelated by default and do not throw exception if draft related is not set
+	 * 
+	 * @param string
+	 * @return Model|null
+	 */
+	public function getRelated($strKey = null)
+	{
+		if($strKey === null)
+		{
+			$strKey = 'draftRelated';
+		}
+		
+		if($strKey == 'draftRelated' && $this->$strKey == 0)
+		{
+			return null;
+		}
+		
+		return parent::getRelated($strKey);		
+	}
+	
+	
+	/**
+	 * check if element has a relation
+	 */
+	public function hasRelated()
+	{
+		return $this->draftRelated > 0;
+	}
+	
+	
+	/**
+	 * check if model has a state
+	 * 
+	 * @param string 
+	 */
+	public function hasState($strState)
+	{
+		switch($strState)
+		{
+			case 'new':
+				return ($this->objModel->draftRelated == '' || $this->objModel->draftRelated == 0);
+				break;
+			
+			case 'sorted':
+				return !$this->hasState('new') && ($this->getRelated()->sorting != $this->objModel->sorting);
+				break;
+			
+			case 'visibility':
+				return !$this->hasState('new') && ($this->getRelated()->invisible != $this->objModel->invisible);
+				break;
+				
+			default:
+				$intFlag = $this->getStateFlag($strState);
+				break;
+		}
+		
+		if($intFlag == 0)
+		{
+			return false;
+		}
+		
+		return (($this->objModel->draftState & $intFlag) == $intFlag);
+	}
+	
+	
+	/**
+	 * check if current model is draft
+	 */
+	public function isDraft()
+	{
+		return $this->hasState('draft');
+	}
+	
+	
+	/**
+	 * create a new model by cloning a reference and replace
+	 * 
+	 * @param bool switch between related and original
+	 * @param bool true if forcing a new model 
+	 * @param bool true is versioning shall be used
+	 */
+	public function prepareCopy($blnSwitch=true, $blnNew=false, $blnVersioning=true)
+	{
+		$objNew = clone $this;
+		$objNew->setVersioning($blnVersioning);
+		$objNew->tstamp = time();
+		
+		// switch ids and draft state
+		if($blnSwitch)
+		{
+			$objNew->id = $blnNew ? null : $this->draftRelated;
+			$objNew->draftRelated = $this->id;
+			
+			if($this->isDraft())
+			{
+				$objNew->draftState = 0;
+			}
+			else
+			{
+				$objNew->setState('draft');
+			}
+		}
+
+		return $objNew;
+	}
+	
+	
+	/**
+	 * semove state from model
+	 * 
+	 * @param string
+	 */
+	public function removeState($strState)
+	{
+		$this->objModel->draftState &= ~$this->getStateFlag($strState);
+	}
+
+
+	/**
+	 * set state of model
+	 * 
+	 * @param string 
+	 */
+	public function setState($strState)
+	{
+		if(!$this->hasState($strState))
+		{
+			$this->objModel->draftState |= $this->getStateFlag($strState);
+		}
+	}
+
+
+	/**
+	 * @param $strState
+	 */
+	public function toggleState($strState)
+	{
+		if($this->hasState($strState))
+		{
+			$this->removeState($strState);
+		}
+		else {
+			$this->setState($strState);
+		}
+	}
+	
+	
+	/**
+	 * get state flag 
+	 * 
+	 * @param string $strState
+	 * @return int
+	 */
+	protected function getStateFlag($strState)
+	{
+		switch ($strState) 
+		{
+			case 'draft':
+				$intFlag = 1;
+				break;
+				
+			case 'modified':
+				$intFlag = 2;
+				break;
+			
+			case 'delete':
+				$intFlag = 4;
+				break;
+				
+			default:
+				$intFlag = 0;
+				break;
+		}
+		
+		return $intFlag;
+	}
+
+}
---
+++ src/modules/drafts/classes/Drafts/Model/VersioningModel.php
@@ -0,0 +1,235 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package   drafts
+ * @author    David Molineus <http://www.netzmacht.de>
+ * @license   GNU/LGPL 
+ * @copyright Copyright 2012 David Molineus netzmacht creative 
+ *  
+ **/
+ 
+namespace Drafts\Model;
+use Controller, Model, Model\Collection, Database\Result;
+
+/**
+ * Versioning Model allows to save model and use the versioning of contao
+ * 
+ */
+class VersioningModel
+{
+	
+	/**
+	 * enable versioning
+	 * 
+	 * @var bool
+	 */
+	protected $blnVersioning;
+	
+	/**
+	 * model reference
+	 * @var \Model
+	 */
+	protected $objModel;
+	
+	/**
+	 * class of collection class
+	 * 
+	 * @var string
+	 */
+	protected static $strCollectionClass = 'VersioningCollection';
+	
+	
+	/**
+	 * create a new model, possible calls
+	 * $obj = new VersioningModel('tl_content');
+	 * $obj = new VersioningModel($objModel);
+	 * $obj = new VersioningModel($objResult, true, 'tl_content');
+	 * 
+	 * @param \Model|Result|string
+	 * @param bool
+	 * @param string
+	 */
+	public function __construct($objModel, $blnVersioning=true, $strTable=null)
+	{
+		// create empty model
+		if(is_string($objModel))
+		{
+			$strClass = \Model::getClassFromTable($objModel);
+			$objModel = new $strClass();
+		}
+		
+		elseif($objModel instanceof Result)
+		{
+			$strClass = \Model::getClassFromTable($strTable);
+			$objModel = new $strClass($objModel);
+		}
+		
+		$this->blnVersioning = $blnVersioning;
+		$this->objModel = $objModel;
+	}
+	
+	
+	/**
+	 * call models method
+	 * 
+	 * @param string method name
+	 * @param array arguments
+	 * @return mixed
+	 */
+	public function __call($strMethod, $arrArguments)
+	{
+		return call_user_func_array(array($this->objModel, $strMethod), $arrArguments);
+	}
+	
+	
+	/**
+	 * create a static call
+	 * First param has to be the table name, so it's possible to decide which model is used
+	 * 
+	 * VersioningModel::findByPK('tl_user', 1);
+	 * 
+	 * @param string $strName
+	 * @param array $arrArguments
+	 *
+	 * @return VersioningModel|VersioningCollection 
+	 */
+	public static function __callStatic($strName, $arrArguments)
+	{
+		$strTable = array_shift($arrArguments);
+		$strClass = \Model::getClassFromTable($strTable);
+		$objModel = call_user_func_array(array($strClass, $strName), $arrArguments);
+		
+		if($objModel === null)
+		{
+			return null;
+		}
+		elseif($objModel instanceof Collection)
+		{
+			return new self::$strCollectionClass($objModel);			
+		}
+		else 
+		{
+			return new static($objModel);
+		}
+	}
+	
+	
+	/**
+	 * clone the model
+	 */
+	public function __clone()
+	{
+		$this->objModel = clone $this->objModel;
+	}
+	
+
+	/**
+	 * get model data
+	 * 
+	 * @param string key
+	 * @return mixed
+	 */
+	public function __get($strKey)
+	{
+		return $this->objModel->$strKey;
+	}
+	
+		
+	/**
+	 * check if model argument isset 
+	 * 
+	 * @param string var name
+	 * @return bool
+	 */
+	public function __isset($strKey)
+	{
+		return isset($this->objModel->$strKey);
+	}
+	
+	
+	/**
+	 * set moodel value
+	 * 
+	 * @param string var name
+	 * @param mixed value
+	 */
+	public function __set($strKey, $mixedValue)
+	{
+		$this->objModel->$strKey = $mixedValue;
+	}
+	
+	
+	/**
+	 * get model
+	 * 
+	 * @return \Model
+	 */
+	public function getModel()
+	{
+		return $this->objModel;
+	}
+	
+	
+	/**
+	 * get related also create a versioning model
+	 * 
+	 * @param string
+	 * @return VersioningModel|null
+	 */
+	public function getRelated($strKey)
+	{
+		$objReturn = $this->objModel->getRelated($strKey);
+		
+		if($objReturn === null)
+		{
+			return null;
+		}
+		
+		return new static($objReturn);
+	}
+	
+	
+	/**
+	 * save model and create model
+	 * 
+	 * @param bool force to inset
+	 * @return VersioningModel
+	 */
+	public function save($blnForceInsert=false, $blnIgnoreVersioning=false)
+	{
+		$strTable = $this->objModel->getTable();
+		$strPk = $this->objModel->getPk();
+		$blnVersioning = $this->blnVersioning && !$blnIgnoreVersioning && $GLOBALS['TL_DCA'][$strTable]['config']['enableVersioning'];
+
+		if($blnVersioning && !$blnForceInsert && isset($this->$strPk))
+		{
+			$objVersions = new \Versions($strTable, $this->$strPk);
+			$objVersions->initialize();
+		}
+		
+		$this->objModel->save($blnForceInsert);
+		
+		if($blnVersioning)
+		{
+			$objVersions->create();
+		}
+		
+		return $this;
+	}
+	
+	
+	/**
+	 * activate or deactive versioning
+	 * 
+	 * @paranm bool
+	 */
+	public function setVersioning($blnEnable)
+	{
+		$this->blnVersioning = (bool) $blnEnable;
+	}
+	
+}
